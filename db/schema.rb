3c57501ec659472a818c99dd52c83edf659644bd
github username <github email address>
2019-03-28 13:49:04 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190326223359) do
+ActiveRecord::Schema.define(version: 2019_03_26_223359) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password"
-    t.float "balance"
+    t.decimal "balance"
   end
 
 end
